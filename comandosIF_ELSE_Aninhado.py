c4731e38ae80ad6abbb1c3457a4dbe3285faea2f
Tiago <tiago@trattecinformatica.com.br>
2024-04-23 15:35:41 -0300

    Python - Condicional Aninhada

---
+++ comandosIF_ELSE_Aninhado.py
@@ -0,0 +1,10 @@
+media = float(input("Digite um número inteiro: "))
+
+if media >= 7.0:
+    print("Aprovado direto.")
+else:
+
+    if media >= 4.0:
+        print("Tem direito a sub.")
+    else:
+        print("Reprovou direto.")
