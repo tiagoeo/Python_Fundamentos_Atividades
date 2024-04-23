afe936429e99136577b004cda644948b50b66d72
Tiago <tiago@trattecinformatica.com.br>
2024-04-23 14:35:37 -0300

    Python - Operadores Aritméticos

---
+++ aritmetica.py
@@ -0,0 +1,15 @@
+A = 2
+B = 3
+Resultado = 0
+
+Resultado = A + B
+print(Resultado)
+
+Resultado = B - A
+print(Resultado)
+
+Resultado = A * B
+print(Resultado)
+
+Resultado = (A + 4) / 3
+print(Resultado)
